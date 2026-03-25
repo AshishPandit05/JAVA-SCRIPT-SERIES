767c0c61b6e2b8a8702fe6bd16451b8cdbdd26b2
AshishPandit05 <mr.ashishpandit05@gmail.com>
2026-03-25 10:56:02 +0530

    Question 1 complete

--- Day-01_Simple_Number_Problems/Solution.js
+++ Day-01_Simple_Number_Problems/Solution.js
@@ -1,1 +1,14 @@
 // 1. Print Numbers from 1 to N
+/* 
++prompt() = shortcut for Number(prompt()) and Converts input from string -> number
+check input ? ---> Number.isInteger(number) && number > 0 
+*/
+
+let num = +prompt("Enter a number :");
+if (Number.isInteger(num) && num > 0) {
+  for (let i = 1; i <= num; i++) {
+    console.log(i);
+  }
+} else {
+  console.log("ERROR");
+}
