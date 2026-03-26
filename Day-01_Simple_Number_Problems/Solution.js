1e1e15c0375d68df23f0d7d9ebca2c1f45eb3d55
AshishPandit05 <mr.ashishpandit05@gmail.com>
2026-03-26 09:42:41 +0530

    question 2 complete

--- Day-01_Simple_Number_Problems/Solution.js
+++ Day-01_Simple_Number_Problems/Solution.js
@@ -12,3 +12,16 @@ if (Number.isInteger(num) && num > 0) {
 } else {
   console.log("ERROR");
 }
+
+// 2. Print Numbers from N to 1 without changing the loop condition of above question
+/* 
+
+*/
+let number = +prompt("Enter a number :");
+if (Number.isInteger(num1) && number > 0) {
+  for (let i = 1; i <= number; i++) {
+    console.log(number - i + 1);
+  }
+} else {
+  console.log("ERROR");
+}
